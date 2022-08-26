90a9e2e7580b4d172a0c8397131d2b71807f0666
realguy33 <70895323+realguy33@users.noreply.github.com>
2022-08-26 19:10:10 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,5 +1,22 @@
 from subgrounds.subgrounds import Subgrounds
 sg = Subgrounds()
 import streamlit as st
+import pandas as pd
 
 st.write('working')
+
+button = st.button('click me')
+if(button == True):
+  # list of name, degree, score 
+  nme = ["aparna", "pankaj", "sudhir", "Geeku"] 
+  deg = ["MBA", "BCA", "M.Tech", "MBA"] 
+  scr = [90, 40, 80, 98] 
+     
+  # dictionary of lists  
+  dict = {'name': nme, 'degree': deg, 'score': scr}  
+       
+  df = pd.DataFrame(dict) 
+    
+  # saving the dataframe 
+  df.to_csv('GFG.csv') 
+  
